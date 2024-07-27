80f1c636631d82682ec55dcfbbdf406e7a8a63cb
aryan <agarwalaryan139@gmail.com>
2024-07-27 18:22:53 +0530

    fix: exit after 5 min

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ dotenv.config();
 const update_score_for_feed = require("./jobs/update_score_for_feed");
 
 update_score_for_feed();
+
+// Force exit after 30 seconds
+setTimeout(() => {
+    console.log('Forcing exit after timeout');
+    process.exit(1);
+  }, 5 * 60000);
