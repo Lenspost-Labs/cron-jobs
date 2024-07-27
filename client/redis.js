6ebe6e01ab0bb6916d718b2b563e73c665289495
aryan <agarwalaryan139@gmail.com>
2024-07-27 18:18:53 +0530

    fix: redis

--- client/redis.js
+++ client/redis.js
@@ -4,4 +4,14 @@ const redis = createClient({
   url: process.env.REDIS_CACHE,
 });
 
+redis.on("error", (err) => {
+  console.log("Redis Caching client error: ", err);
+});
+
+redis.on("connect", () => {
+  console.log("Redis client connected");
+});
+
+redis.connect();
+
 module.exports = redis;
